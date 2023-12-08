321bac8d46c337077e5051d91354dd6bf4e0b138
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2023-12-08 12:43:33 +0530

    Add files via upload

---
+++ conditional_statement.py
@@ -0,0 +1,11 @@
+# conditional statement
+a = 8
+b = 9
+if a>b:
+    print("Hi I am from if block")
+elif a>=b:
+    print("Hi I am from elif block")
+elif a>=5:
+    print("Hi I am 2nd elif block")
+else:
+    print("Hi I am from else block")
